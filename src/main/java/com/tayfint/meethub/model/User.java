ccace3c637a3ca37fd8ca1f64590daed93c166ca
Yvan K Guiatheu <yguiathe@gmail.com>
2018-08-01 23:12:45 -0700

    Fixes User model by unlocking user by default

--- src/main/java/com/tayfint/meethub/model/User.java
+++ src/main/java/com/tayfint/meethub/model/User.java
@@ -465,7 +465,7 @@ public class User implements UserDetails {
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
