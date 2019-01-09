7bec42d94676657129b365e9eb01d362263339b3
Yvan K Guiatheu <yguiathe@gmail.com>
2019-01-08 18:57:47 -0800

    Refactors vertical navbar

--- src/main/webapp/static/js/app_financial.js
+++ src/main/webapp/static/js/app_financial.js
@@ -106,12 +106,12 @@ function display(data) {
 
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
 var dropdown = document.getElementsByClassName("dropdown-btn");
-document.getElementById("top-menu").style.color = "white";
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("main-color-bg");
+	  document.getElementById("top-menu").style.color = "white";
+	  this.classList.toggle("main-color-bg");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
