944960e43f8c5390b4f7559f9b94f10956b00de7
Yvan K Guiatheu <yguiathe@gmail.com>
2019-05-22 00:44:38 -0700

    Implements transaction form

--- src/main/webapp/static/js/app_financial.js
+++ src/main/webapp/static/js/app_financial.js
@@ -139,7 +139,6 @@ function getAccountDetails() {
 		$('#myAccounts').removeAttr('style');
 		$('#transactionModal').on('show.bs.modal', function(event) {
 			var button = $(event.relatedTarget);
-			console.log(event);
 			var title = button.data('whatever'); // Extract info from data-*
 													// attributes
 			// If necessary, you could initiate an AJAX request here (and then do the
@@ -147,7 +146,6 @@ function getAccountDetails() {
 			// Update the modal's content. We'll use jQuery here, but you could use a
 			// data binding library or other methods instead.
 			var modal = $(this);
-			console.log(modal);
 			modal.find('.modal-title').text(title);
 			// modal.find('.modal-body input').val(recipient);
 		});
